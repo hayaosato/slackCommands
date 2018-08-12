44c8f82ed3bc60345893ea52f86168bf9aa386cb
hayaosato <jake.bibikyary.880@gmail.com>
2018-08-12 11:36:18 +0900

    hundle parameters

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 post '/slack/command' do
-  "hoge"
+  #index_word = params[:text]
+  # ruby_search(index_word)
+  parameters = "```\n" + params[:token] + "\n"
+  parameters += params[:command] + "\n"
+  parameters += params[:text] + "\n"
+  parameters += params[:response_url] + "\n"
+  parameters += params[:trigger_id] + "\n"
+  parameters += params[:user_id] + "\n"
+  parameters += params[:user_name] + "\n"
+  parameters += params[:team_id] + "\n"
+  parameters += params[:channel_id] + "\n```"
+end
+
+def ruby_search(index_word)
+  out_put_text = "#{index_word}について検索しました。"
+  out_put_text
 end
